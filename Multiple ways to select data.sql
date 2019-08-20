ab968e0f637cbbc8ec594cecb3134bf47f50feae
Nicholas Baker <Nicholasbaker7010@gmail.com>
2019-08-20 13:50:52 -0400

    Added context to script

--- Multiple ways to select data.sql
+++ Multiple ways to select data.sql
@@ -1,8 +1,13 @@
---select * from Customers;
---select name, city, state from Customers;
-/*select * from customers
+-- get all rows and columns from customers table
+select * from Customers;
+
+-- get city, state, id, and name from customers table
+select name, city, state from Customers;
+
+--get all columns from Ohio, Indiana, and Kentucky
+select * from customers
 	where state = 'OH' or state = 'IN' or state = 'KY' 
 		order by state desc, city;
-*/
+-- get all columns for Jersey City
 select * from Customers
 	where City = 'Jersey city';
